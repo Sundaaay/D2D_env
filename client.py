58b6b8c243265ad3d0b7b264051ccc4f0b3f45e4
Sundaaay <987794678@qq.com>
2017-06-09 09:58:04 +0800

    Create client.py

--- client.py
+++ client.py
@@ -12,20 +12,20 @@ print('I am',str(client_name,"utf-8"))
 while True:
 
 
-    #选择需要文件
+    
     print('Please input the name of the file you want:')
     filename = input()
     s.sendall(bytes(filename,"utf-8"))
     file_object = open("Sever_"+filename,'w')
 
-    #选择发送端
+    
     print('Please choose a client which could send the file to you!')
     clients_with_file = s.recv(4096)
     print(str(clients_with_file,"utf-8"))
     choosed_client = input()
     s.sendall(bytes(choosed_client,"utf-8"))
     
-    #文件写入
+    
     file_data = s.recv(4096)
     file_object.write(str(file_data,"utf8"))
     file_object.close()
