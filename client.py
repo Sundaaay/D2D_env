a9e9468b38cca7dc2e3f48e75503d9b62e68a794
TheOldCaptain <1016renyan@gmail.com>
2017-07-23 16:56:02 +0800

    将客户端和服务端具体到了某一文件夹
    整个过程中没有文件的传输，没有加入位置关系。
    还没有使用过docker进行测试

--- client.py
+++ client.py
@@ -1,36 +1,56 @@
 import socket
 import time
+import os
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = '127.0.0.1'
 port = 8888
+position = (1234, 5678)
+client.connect((host, port))
 
-s.connect((host,port))
-client_name = s.recv(4096)
-print('I am',str(client_name,"utf-8"))
+
+def package(message,size):
+    new_message = message
+    while len(bytes(new_message, 'utf-8')) < size:
+        new_message = new_message + '-'
+    return new_message
+
+
 while True:
+    # 1、读取该文件夹下的文件,将文件列表发送至服务器
+    file_dir = os.listdir('F://Client')
+    client.sendall(bytes(package('$'.join(file_dir), 1024), 'utf-8'))
+    print('sended file_dir!')
 
+    # 接收来自服务器的包含文件信息的字典显示出来  这里你可能改成命名元组或者其他
+    recv_file_data = client.recv(1024)
+    data = str(recv_file_data, 'utf-8').rstrip('-')
+    recv_file_dict = eval(data)
+    for i, j in recv_file_dict.items():
+        print('{}里的文件有: '.format(i), end='')
+        for m in j:
+            print('{}    '.format(m), end='')
+        print('\n')
+    print('*' * 20)
 
-    
-    print('Please input the name of the file you want:')
-    filename = input()
-    s.sendall(bytes(filename,"utf-8"))
-    
+    # 2、用户输入想获取的文件
+    filename = input('Please input the name of the file you want:')
 
-    
-    print('Please choose a client which could send the file to you!')
-    clients_with_file = s.recv(4096)
-    print(str(clients_with_file,"utf-8"))
-    choosed_client = input()
-    s.sendall(bytes(choosed_client,"utf-8"))
-    
-    file_object = open(choosed_client+"_"+filename,'w')
-    file_data = s.recv(4096)
-    file_object.write(str(file_data,"utf8"))
-    file_object.close()
+    client.sendall(bytes(package(filename, 1024), 'utf-8'))
+
+    print('*' * 20)
+
+    # 3、服务器向客户端返回所有包含这个文件的节点信息的一个列表
+    contain_file_list = str(client.recv(1024), 'utf-8').rstrip('-').split('$')
+    print(contain_file_list)
+    for i in contain_file_list:
+        print('{}----{}'.format(contain_file_list.index(i), i))
+    print('*'*20)
+
+    # 4、用户选择一个节点在列表中的下标
+    choosed_client = int(input('Please choose a client which could send the file to you:'))
+    client.sendall(bytes(package(contain_file_list[choosed_client], 60), "utf-8"))
     print("Successfully get the file !")
-    
-    
 
 s.close()
--- server.py
+++ server.py
@@ -1,71 +1,81 @@
 import socketserver
-
-i=0
-
-
+import socket
+import os
 
 data = {}
-
 trans_data = {}
+file_dict = {}
+
+
+def package(message, size):
+    new_message = message
+    while len(bytes(new_message, 'utf-8')) < size:
+        new_message = new_message + '-'
+    return new_message
+
+
+def find_file_host(file_dict, filename):
+        host_list = list()
+        for i, j in file_dict.items():
+                for m in j:
+                        if m == filename:
+                                host_list.append(i)
+        return host_list
+
+def add_file(file_dict, file_send_host, req_filename):
+        for i in file_dict[file_send_host]:
+                if req_filename == i:
+                        return
+        file_dict[file_send_host] = tuple(list(file_dict[file_send_host]).append(req_filename))
+
+
 class myTCPHandler(socketserver.BaseRequestHandler):
         
         def handle(self):
-                
-                clients = []
-                clients_with_file = ['base_station']
-                global i
-                
-                client_name = 'C'+str(i)
-                self.request.sendall(bytes(client_name ,encoding = "utf8"))
-                
-                client_file = ('file'+str(i)+'.txt','file'+str(i+1)+'.txt','file'+str(i+2)+'.txt')
-                
-                data[client_name] = client_file
-                i += 1
-                
-                clients.append(client_name)
-                print('get connection from',self.client_address,'as',client_name,data)
                 while 1:
-                        self.data = self.request.recv(1024)
-                        filename = str(self.data,encoding = "utf-8")
-                        
-                        
+                        # 从本地读取某一个文件夹中的文件数据当作基站所拥有的文件
+                        file_list = os.listdir('F://Server')
+                        file_dict[socket._LOCALHOST] = tuple(file_list)
+
+                        # 1、接收客户端发送过来的文件列表,添加到file_dict字典中
+                        client_data = self.request.recv(1024)
+                        print('receive file_dict from client:{}'.format(self.client_address))
+                        data = str(client_data, 'utf-8').rstrip('-')
+                        client_file_list = data.split('$')
+                        file_dict[self.client_address[0]] = tuple(client_file_list)
+                        print('update file_dict!')
+                        for i, j in file_dict.items():
+                                print('{}里的文件有: '.format(i), end='')
+                                for m in j:
+                                        print('{}    '.format(m), end='')
+                                print('\n')
+                        print('*'*20)
+
+                        # 2、将服务器中的file_dict 发送发客户端 因为字典转换成字符串后在客户端无法将这个字符串转换成字典
+                        self.request.sendall(bytes(package(str(file_dict), 1024), 'utf-8'))
+                        print('sended file_dict to Client!')
+                        print('*'*20)
+
+                        # 3、接收客户端发来的所请求的文件名
+                        filename_data = self.request.recv(1024)
+                        req_filename = str(filename_data, 'utf-8').rstrip('-')
+                        print('cilent choose a file:  {}'.format(req_filename))
+                        print('*'*20)
+
+                        # 4、在file_dict 的 value中查找这个文件，返回一个有这个文件的主机的ip 的列表
+                        host_have_thefile = find_file_host(file_dict, req_filename)
+
+                        # 5、将这个主机列表发送给请求的客户机
+                        self.request.sendall(bytes(package('$'.join(host_have_thefile), 1024), 'utf-8'))
+
+                        # 6、接收客户选择的主机
+                        file_send_host = str(self.request.recv(60), 'utf-8').rstrip('-')
+                        print(file_send_host)
+
+                        add_file(file_dict, file_send_host, req_filename)
 
                         
-                        keys = list(data.keys())
-                        for m in range(len(keys)):
-                            for n in range(len(data[keys[m]])):
-                                if keys[m] == client_name:
-                                        continue
-                                if data[keys[m]][n] == filename:
-                                    clients_with_file.append(keys[m])
 
-                        
-                        self.request.sendall(bytes(str(clients_with_file),encoding = "utf8"))
-                        self.data = self.request.recv(1024)
-                        
-                        
-                        choosed_name = str(self.data,encoding = "utf-8")
-                                 
-                        file_object = open(filename)
-                        try:
-                                file_data = file_object.read( )
-                        finally:
-                                file_object.close( )
-                        print (choosed_name,'has send',filename,'to',self.client_address,client_name)
-                        self.request.sendall(bytes(file_data ,encoding = "utf8"))
-
-                        
-                        if choosed_name in trans_data.keys():
-                                trans_data[choosed_name] = trans_data[choosed_name] + tuple([client_name,filename])
-                        else:
-                                trans_data[choosed_name] = tuple([client_name,filename])
-                        print("The historical information:",trans_data)
-
-                       
-                        clients_with_file = ['base_station']
-                        
-
-h,p = '',8888
+h,p = '', 8888
 server = socketserver.ThreadingTCPServer((h,p),myTCPHandler)
 server.serve_forever()
