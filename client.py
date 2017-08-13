fae92575e668f3e2afbc992d09c6678fad4080f5
TheOldCaptain <1016renyan@gmail.com>
2017-08-13 09:04:24 +0800

    auto-request
    添加了自动球求，以及传输历史写入文件

--- client.py
+++ client.py
@@ -1,56 +1,116 @@
-import socket
-import time
 import os
+import random
+import socket
+import threading
+import time
+
+from trans_file import trans_file_Client, trans_file_Server
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 host = '127.0.0.1'
 port = 8888
 position = (1234, 5678)
 client.connect((host, port))
+FILE_DIR = 'F:/Client'
 
-
-def package(message,size):
+def package(message, size):
     new_message = message
     while len(bytes(new_message, 'utf-8')) < size:
         new_message = new_message + '-'
     return new_message
 
 
-while True:
-    # 1、读取该文件夹下的文件,将文件列表发送至服务器
-    file_dir = os.listdir('F://Client')
-    client.sendall(bytes(package('$'.join(file_dir), 1024), 'utf-8'))
-    print('sended file_dir!')
+def generate_position():
+    """
+    generate the client`s position
+    :return: postion like (x, y)
+    """
+    position = (random.uniform(1, 200), random.uniform(1, 200))
+    return position
 
-    # 接收来自服务器的包含文件信息的字典显示出来  这里你可能改成命名元组或者其他
+
+def list_append(choose_list, j):
+    for i in choose_list:
+        if j == i:
+            return
+    choose_list.append(j)
+
+
+def show_file_inf():
+    choose_list = list()
     recv_file_data = client.recv(1024)
     data = str(recv_file_data, 'utf-8').rstrip('-')
-    recv_file_dict = eval(data)
+    print(data)
+    recv_file_dict = eval(data)     # 将字典的字符串转化成字典
     for i, j in recv_file_dict.items():
         print('{}里的文件有: '.format(i), end='')
         for m in j:
+            if m == '':
+                continue
             print('{}    '.format(m), end='')
+            list_append(choose_list, m)
         print('\n')
     print('*' * 20)
+    return choose_list
 
-    # 2、用户输入想获取的文件
-    filename = input('Please input the name of the file you want:')
 
-    client.sendall(bytes(package(filename, 1024), 'utf-8'))
+def choose_file(choose_list, file_dir):
+    random_num = random.randint(0, len(choose_list) - 1)
+    while choose_list[random_num] in file_dir:
+        if len(choose_list) == len(file_dir):
+            return -1
+        print('you had the file in you directory please rechoose!')
+        random_num = random.randint(0, len(choose_list) - 1)
+    return choose_list[random_num]
 
-    print('*' * 20)
 
-    # 3、服务器向客户端返回所有包含这个文件的节点信息的一个列表
-    contain_file_list = str(client.recv(1024), 'utf-8').rstrip('-').split('$')
-    print(contain_file_list)
-    for i in contain_file_list:
-        print('{}----{}'.format(contain_file_list.index(i), i))
-    print('*'*20)
+def request():
+    while True:
 
-    # 4、用户选择一个节点在列表中的下标
-    choosed_client = int(input('Please choose a client which could send the file to you:'))
-    client.sendall(bytes(package(contain_file_list[choosed_client], 60), "utf-8"))
-    print("Successfully get the file !")
+        # 请求的概率是无法确定的，所以我先采用的是概率为50%，即每个客户端到这里都有50%的概率会随机请求一个文件
+        if random.randint(0, 9) >= 5:
+            print('i will not send a request!')
+            time.sleep(5)
+            continue
 
-s.close()
+        # 生成client所在的位置信息
+        position = generate_position()
+        client.sendall(bytes(package(str(position), 1024), 'utf-8'))
+        print('sended position...')
+        print('*' * 20)
+
+        # 读取该文件夹下的文件,将文件列表发送至服务器
+        file_dir = os.listdir(FILE_DIR)
+        client.sendall(bytes(package('$'.join(file_dir), 1024), 'utf-8'))
+        print('sended file_dir...')
+        print('*' * 20)
+
+        # 接收来自服务器的包含文件信息的字典显示出来  这里你可能改成命名元组或者其他
+        choose_list = show_file_inf()
+
+        # 随机选择一个文件进行传输,
+        print('可以选择的文件有{}'.format(choose_list))
+        random_file = choose_file(choose_list, file_dir)
+        if random_file == -1:                                               # 返回 -1 表示该节点中已经拥有了所有节点的文件了
+            client.sendall(bytes(package('have_all_file', 1024), 'utf-8'))
+            print('contained all file in the system!')
+            continue
+        client.sendall(bytes(package(random_file, 1024), 'utf-8'))
+
+        print('*' * 20)
+
+        # 3、服务器向客户端返回所有包含这个文件的节点信息的一个列表
+        best_sender = str(client.recv(1024), 'utf-8').rstrip('-')
+        if best_sender == '404':
+            print('this file is not in this System!')
+            continue
+        else:
+            print('the best sender is {}'.format(best_sender))
+            fp = open(FILE_DIR+'/'+random_file, "w")
+            fp.close()
+        print('*' * 20)
+
+
+thread_request = threading.Thread(target=request(), args=())
+thread_request.start()
+client.close()
--- server.py
+++ server.py
@@ -1,10 +1,14 @@
-import socketserver
-import socket
 import os
+import socketserver
+import threading
 
-data = {}
-trans_data = {}
+
 file_dict = {}
+host_position = {}
+BASE_LOCALHOST = '0.0.0.0'
+BASE_POSITION = (34, 56)
+FILE_DIR = 'F:/Server'
+HISTORY_DIR = 'F:/file_tans_history.txt'
 
 
 def package(message, size):
@@ -14,68 +18,103 @@ def package(message, size):
     return new_message
 
 
-def find_file_host(file_dict, filename):
-        host_list = list()
-        for i, j in file_dict.items():
-                for m in j:
-                        if m == filename:
-                                host_list.append(i)
-        return host_list
+def find_file_host(file_dict, filename, client_address):
+    host_list = list()
+    for i, j in file_dict.items():
+        for m in j:
+            if m == filename and i != client_address:
+                host_list.append(i)
+    return host_list
 
-def add_file(file_dict, file_send_host, req_filename):
-        for i in file_dict[file_send_host]:
-                if req_filename == i:
-                        return
-        file_dict[file_send_host] = tuple(list(file_dict[file_send_host]).append(req_filename))
+
+def choose_best_sender(host_position, host_have_the_file, request_ip):
+    if len(host_have_the_file) == 0:
+        print('there is not the file in the System!')
+        return -1
+    p1 = host_position[request_ip]
+    distance_dict = dict()
+    for name in host_have_the_file:
+        p = host_position[name]
+        distance_dict[name] = (pow(p1[0] - p[0], 2) + pow(p1[1] - p[1], 2)) ** 0.5
+    best_host = host_have_the_file[0]
+    for name, dis in distance_dict.items():
+        if dis <= distance_dict[best_host]:
+            best_host = name
+    print('the best sender is {}'.format(best_host))
+    return best_host
+
+
+def write_trans_history(req_host, send_host, file_name):
+    fp = open(HISTORY_DIR, 'a')
+    fp.write(send_host + ':' + str(host_position[send_host]) + '-->' + req_host + ':' + str(
+        host_position[req_host]) + ':' + file_name + '\n')
+    fp.close()
 
 
 class myTCPHandler(socketserver.BaseRequestHandler):
-        
-        def handle(self):
-                while 1:
-                        # 从本地读取某一个文件夹中的文件数据当作基站所拥有的文件
-                        file_list = os.listdir('F://Server')
-                        file_dict[socket._LOCALHOST] = tuple(file_list)
+    def handle(self):
+        while 1:
+            # 从本地读取某一个文件夹中的文件数据当作基站所拥有的文件
+            file_list = os.listdir(FILE_DIR)
+            file_dict[BASE_LOCALHOST] = tuple(file_list)
+            host_position[BASE_LOCALHOST] = BASE_POSITION
 
-                        # 1、接收客户端发送过来的文件列表,添加到file_dict字典中
-                        client_data = self.request.recv(1024)
-                        print('receive file_dict from client:{}'.format(self.client_address))
-                        data = str(client_data, 'utf-8').rstrip('-')
-                        client_file_list = data.split('$')
-                        file_dict[self.client_address[0]] = tuple(client_file_list)
-                        print('update file_dict!')
-                        for i, j in file_dict.items():
-                                print('{}里的文件有: '.format(i), end='')
-                                for m in j:
-                                        print('{}    '.format(m), end='')
-                                print('\n')
-                        print('*'*20)
+            # 0、接收客户端发送过来的位置信息，保存到 host_position字典中
+            client_data = self.request.recv(1024)
+            data = str(client_data, 'utf-8').rstrip('-')
+            host_position[self.client_address[0]] = eval(data)
+            for i, j in host_position.items():
+                print('{}:{}\n'.format(i, j))
 
-                        # 2、将服务器中的file_dict 发送发客户端 因为字典转换成字符串后在客户端无法将这个字符串转换成字典
-                        self.request.sendall(bytes(package(str(file_dict), 1024), 'utf-8'))
-                        print('sended file_dict to Client!')
-                        print('*'*20)
+            # 1、接收客户端发送过来的文件列表,添加到file_dict字典中
+            client_data = self.request.recv(1024)
+            print('receive file_dict from client:{}'.format(self.client_address))
+            data = str(client_data, 'utf-8').rstrip('-')
+            client_file_list = data.split('$')
+            file_dict[self.client_address[0]] = tuple(client_file_list)
+            print('update file_dict!')
+            for i, j in file_dict.items():
+                print('{} 里的文件有: '.format(i), end='')
+                for m in j:
+                    print('{}    '.format(m), end='')
+                print('\n')
+            print('*' * 20)
 
-                        # 3、接收客户端发来的所请求的文件名
-                        filename_data = self.request.recv(1024)
-                        req_filename = str(filename_data, 'utf-8').rstrip('-')
-                        print('cilent choose a file:  {}'.format(req_filename))
-                        print('*'*20)
+            # 2、将服务器中的file_dict 发送发客户端 因为字典转换成字符串后在客户端无法将这个字符串转换成字典
+            self.request.sendall(bytes(package(str(file_dict), 1024), 'utf-8'))
+            print('sended file_dict to Client:  {}...'.format(self.client_address[0]))
+            print('*' * 20)
 
-                        # 4、在file_dict 的 value中查找这个文件，返回一个有这个文件的主机的ip 的列表
-                        host_have_thefile = find_file_host(file_dict, req_filename)
+            # 3、接收客户端发来的所请求的文件名
+            filename_data = self.request.recv(1024)
+            req_filename = str(filename_data, 'utf-8').rstrip('-')
+            if req_filename == 'have_all_file':
+                continue
+            print('client:  {} choose a file:  {}'.format(self.client_address[0], req_filename))
+            print('*' * 20)
 
-                        # 5、将这个主机列表发送给请求的客户机
-                        self.request.sendall(bytes(package('$'.join(host_have_thefile), 1024), 'utf-8'))
+            # 4、在file_dict 的 value中查找这个文件，返回一个有这个文件的主机的ip 的列表
+            host_have_the_file = find_file_host(file_dict, req_filename, self.client_address[0])
 
-                        # 6、接收客户选择的主机
-                        file_send_host = str(self.request.recv(60), 'utf-8').rstrip('-')
-                        print(file_send_host)
+            # 在包含了该文件的主机列表中选择，距离请求主机最近的主机作为最优文件发送者
+            best_sender = choose_best_sender(host_position, host_have_the_file, self.client_address[0])
 
-                        add_file(file_dict, file_send_host, req_filename)
+            # 如果该系统中没有这个文件就返回 404 错误，若有就将这个主机发送给请求主机。
+            if best_sender == -1:
+                print('find file error 404 !')
+                self.request.sendall(bytes(package('404', 1024), 'utf-8'))
+                continue
+            else:
+                self.request.sendall(bytes(package(best_sender, 1024), 'utf-8'))
+            write_trans_history(self.client_address[0], best_sender, req_filename)
 
-                        
 
-h,p = '', 8888
-server = socketserver.ThreadingTCPServer((h,p),myTCPHandler)
-server.serve_forever()
+def deal_request():
+    h, p = '', 8888
+    server = socketserver.ThreadingTCPServer((h, p), myTCPHandler)
+    server.serve_forever()
+
+
+thread_del_request = threading.Thread(target=deal_request(), args=())
+thread_del_request.start()
+
